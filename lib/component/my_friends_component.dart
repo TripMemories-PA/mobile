a922154682ef7c560361e09f81dfcaae66a17fff
Noé Marleix <noe.marleix@procomm-mmc.com>
2024-04-26 21:14:16 +0200

    friend list

--- lib/component/my_friends_component.dart
+++ lib/component/my_friends_component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../constants/my_colors.dart';
 import 'custom_card.dart';
@@ -10,28 +11,97 @@ class MyFriendsComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
       children: [
-        CustomCard(
-          width: MediaQuery.of(context).size.width * 0.40,
-          heigth: 40,
-          content: const Text(
-            'Ajouter un amis',
-            textAlign: TextAlign.center,
-          ),
-          borderColor: MyColors.purple,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CustomCard(
+              width: MediaQuery.of(context).size.width * 0.40,
+              heigth: 40,
+              content: const Text(
+                'Ajouter un amis',
+                textAlign: TextAlign.center,
+              ),
+              borderColor: MyColors.purple,
+            ),
+            CustomCard(
+              width: MediaQuery.of(context).size.width * 0.40,
+              heigth: 40,
+              content: const Text(
+                'Gérer les demandes',
+                textAlign: TextAlign.center,
+              ),
+              borderColor: MyColors.purple,
+            ),
+          ],
         ),
-        CustomCard(
-          width: MediaQuery.of(context).size.width * 0.40,
-          heigth: 40,
-          content: const Text(
-            'Gérer les demandes',
-            textAlign: TextAlign.center,
-          ),
-          borderColor: MyColors.purple,
+        const SizedBox(
+          height: 20,
         ),
+        _buildFriendsList(context),
       ],
+    );
+  }
+
+  Widget _buildFriendsList(BuildContext context) {
+    return Column(
+      children: List.generate(15, (index) {
+        return Column(
+          children: [
+            _buildFriendCard(context),
+            const SizedBox(height: 10),
+          ],
+        );
+      }),
+    );
+  }
+
+
+  CustomCard _buildFriendCard(BuildContext context) {
+    return CustomCard(
+      width: MediaQuery.of(context).size.width * 0.90,
+      heigth: 55,
+      borderColor: MyColors.lightGrey,
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              const SizedBox(width: 12,),
+              SizedBox(
+                width: 40,
+                height: 40,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(50.0),
+                  ),
+                  child: Image.asset('assets/images/profileSample.png'),
+                ),
+              ),
+              const SizedBox(width: 10),
+              const Column(
+                children: [
+                  Text(
+                    'Jane Doe',
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '@jane_doe',
+                    style: TextStyle(fontSize: 10, color: Colors.grey),
+                  ),
+                ],
+              ),
+              const Expanded(child: SizedBox()),
+              IconButton(onPressed: () => print('coucou'), icon: const Icon(Icons.chat)),
+              const SizedBox(width: 5),
+              IconButton(onPressed: () => print('coucou'), icon: const Icon(Icons.remove_red_eye)),
+              const SizedBox(width: 20),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/constants/my_colors.dart
+++ lib/constants/my_colors.dart
@@ -2,4 +2,5 @@ import 'dart:ui';
 
 class MyColors {
   static const purple = Color(0xFF4F52BD);
+  static const lightGrey = Color(0x80454D5B);
 }

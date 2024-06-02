132848e2db102bdfecf48fd7787bb41592178976
noeoxycode <noe@marleix.com>
2024-06-02 23:27:04 +0200

    wip

--- lib/component/profile_banner.dart
+++ lib/component/profile_banner.dart
@@ -9,7 +9,6 @@ import 'bouncing_widget.dart';
 import 'custom_card.dart';
 import 'popup/confirmation_logout_dialog.dart';
 import 'popup/modify_user_infos_popup.dart';
-import 'profile_picture.dart';
 
 class ProfileBanner extends StatelessWidget {
   const ProfileBanner({
@@ -23,94 +22,52 @@ class ProfileBanner extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ProfileBloc, ProfileState>(
       builder: (context, state) {
-        return SizedBox(
+        return Container(
           width: MediaQuery.of(context).size.width,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.background,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: Column(
             children: [
-              ProfilePicture(
-                isMyProfile: isMyProfile,
-              ),
-              Column(
-                children: [
-                  const SizedBox(height: 30, width: 25),
-                  Row(
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            '${context.read<ProfileBloc>().state.profile?.firstname ?? 'User'} ${context.read<ProfileBloc>().state.profile?.lastname ?? context.read<ProfileBloc>().state.profile?.id.toString()}',
-                            style: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '@${context.read<ProfileBloc>().state.profile?.username}',
-                            style: const TextStyle(
-                              fontSize: 15,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
+              SizedBox(height: 65, child: Align(
+                alignment: Alignment.centerLeft,
+                child: BouncingWidget(
+                  onTap: () async {
+                    await modifyUserInfosPopup(context);
+                  },
+                  child: const CustomCard(
+                    width: 100,
+                    height: 25,
+                    content: Text(
+                      'Editer',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
                       ),
-                      const SizedBox(width: 10),
-                      if (isMyProfile)
-                        Column(
-                          children: [
-                            BouncingWidget(
-                              onTap: () async {
-                                final bool result = await confirmationLogout(
-                                  context,
-                                  title:
-                                      'Etes-vous sûr de vouloir vous déconnecter ?',
-                                );
-                                if (!result) {
-                                  return;
-                                } else {
-                                  if (context.mounted) {
-                                    context.read<AuthBloc>().add(
-                                          const ChangeToLoggedOutStatus(),
-                                        );
-                                  }
-                                }
-                              },
-                              child: const CustomCard(
-                                width: 50,
-                                height: 25,
-                                backgroundColor: Colors.red,
-                                content: Icon(
-                                  Icons.logout,
-                                  color: Colors.white,
-                                  size: 15,
-                                ),
-                                borderColor: Colors.transparent,
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            BouncingWidget(
-                              onTap: () async {
-                                await modifyUserInfosPopup(context);
-                              },
-                              child: const CustomCard(
-                                width: 100,
-                                height: 25,
-                                content: Text(
-                                  'Editer',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                borderColor: Colors.transparent,
-                                backgroundColor: MyColors.purple,
-                              ),
-                            ),
-                          ],
-                        ),
-                    ],
+                    ),
+                    borderColor: Colors.transparent,
+                    backgroundColor: MyColors.purple,
                   ),
-                ],
+                ),
+              ),),
+              Text(
+                '${context.read<ProfileBloc>().state.profile?.firstname ?? 'User'} ${context.read<ProfileBloc>().state.profile?.lastname ?? context.read<ProfileBloc>().state.profile?.id.toString()}',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '@${context.read<ProfileBloc>().state.profile?.username}',
+                style: const TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey,
+                ),
               ),
             ],
           ),

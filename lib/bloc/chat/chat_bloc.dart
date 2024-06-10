81fce26d91c52569084a178808590efaa7983a2a
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2024-06-10 23:56:06 +0200

    chat page affichée

--- lib/bloc/chat/chat_bloc.dart
+++ lib/bloc/chat/chat_bloc.dart
@@ -10,18 +10,21 @@ part 'chat_state.dart';
 class ChatBloc extends Bloc<ChatEvent, ChatState> {
   ChatBloc() : super(const ChatState()) {
     on<GetConversationEvent>((event, emit) {
-      // TODO: implement event handler
-      List<Message> messages = [message];
+      final List<Message> messages = [message];
       for (int i = 0; i < 10; i++) {
         messages.add(message);
         messages.add(myMessage);
       }
+      messages.add(otherMessage);
+      messages.add(otherMessage2);
+      final List<Message> sortedMessages = List.from(messages);
+      sortedMessages.sort((a, b) => b.sentAt.compareTo(a.sentAt));
       emit(
         state.copyWith(
           conversation: Conversation(
             id: 1,
             users: const [profile, profile],
-            messages: messages,
+            messages: sortedMessages,
           ),
         ),
       );
@@ -29,7 +32,7 @@ class ChatBloc extends Bloc<ChatEvent, ChatState> {
   }
 }
 
-const Profile profile = Profile(
+const Profile me = Profile(
   isFriend: true,
   id: 1,
   username: 'johndoe',
@@ -40,18 +43,49 @@ const Profile profile = Profile(
   isReceivedFriendRequest: false,
 );
 
+const Profile profile = Profile(
+  isFriend: true,
+  id: 2,
+  username: 'johndoe',
+  firstname: 'John',
+  lastname: 'Doe',
+  email: 'test@mail.com',
+  isSentFriendRequest: false,
+  isReceivedFriendRequest: false,
+);
+
 final Message message = Message(
   id: 2,
-  message: 'Hello',
+  message:
+      'Other message edrse neiurvn eurnepu e u pevqp npu hpzue qzpmun zpqu eijbf bmefn zj',
   sender: profile,
-  receiver: profile,
+  receiver: me,
   sentAt: DateTime.now(),
 );
 
 final Message myMessage = Message(
   id: 1,
-  message: 'Hello',
-  sender: profile,
+  message:
+      'My message rmopgijergijzrpigpoo gj erpoon eepiu vie e  irpaiirn ilorb riub e bvqb q olhfvbahb eh ve pmzh apmiujfnijb ',
+  sender: me,
   receiver: profile,
   sentAt: DateTime.now(),
+);
+
+final Message otherMessage = Message(
+  id: 1,
+  message:
+      'My message rmopgijergijzrpigpoo gj erpoon eepiu vie e  irpaiirn ilorb riub e bvqb q olhfvbahb eh ve pmzh apmiujfnijb ',
+  sender: me,
+  receiver: profile,
+  sentAt: DateTime.utc(1969, 7, 20, 20, 18, 04),
+);
+
+final Message otherMessage2 = Message(
+  id: 1,
+  message:
+      'My message rmopgijergijzrpigpoo gj erpoon eepiu vie e  irpaiirn ilorb riub e bvqb q olhfvbahb eh ve pmzh apmiujfnijb ',
+  sender: me,
+  receiver: profile,
+  sentAt: DateTime.utc(1970, 7, 20, 20, 18, 04),
 );
--- lib/page/chat_page.dart
+++ lib/page/chat_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../bloc/auth_bloc/auth_bloc.dart';
+import '../bloc/chat/chat_bloc.dart';
 import '../component/profile_picture.dart';
 import '../constants/my_colors.dart';
 import '../num_extensions.dart';
@@ -12,43 +15,240 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final int myId = context.read<AuthBloc>().state.user?.id ?? 0;
     return SafeArea(
       child: Scaffold(
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        body: BlocProvider(
+          create: (context) => ChatBloc()
+            ..add(
+              GetConversationEvent(
+                isRefresh: true,
+                conversationId: 1,
+              ),
+            ),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                10.ph,
+                _buildHeader(),
+                Expanded(
+                  child: BlocBuilder<ChatBloc, ChatState>(
+                    builder: (context, state) {
+                      return Container(
+                        alignment: Alignment.bottomCenter,
+                        child: _buildChatMessages(state, myId, context),
+                      );
+                    },
+                  ),
+                ),
+                _buildMessageTextInput(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding _buildMessageTextInput(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.tertiary,
+                ),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Ecrire un message',
+                  hintStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.tertiary,
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column _buildHeader() {
+    return Column(
+      children: [
+        ProfilePicture(
+          uploadedFile: user.avatar,
+        ),
+        10.ph,
+        Text(
+          '${user.firstname} ${user.lastname}',
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '@${user.username}',
+          style: const TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Container(
+            color: MyColors.darkGrey,
+            height: 1,
+          ),
+        ),
+      ],
+    );
+  }
+
+  ListView _buildChatMessages(ChatState state, int myId, BuildContext context) {
+    return ListView(
+      reverse: true,
+      children: [
+        for (int i = 0;
+            i < (state.conversation?.messages.length ?? 0);
+            i++) ...[
+          if (i > 0 &&
+              !_isSameDay(
+                state.conversation?.messages[i - 1].sentAt,
+                state.conversation?.messages[i].sentAt,
+              ))
+            _buildDaySeparator(context, state.conversation?.messages[i].sentAt),
+          Row(
+            mainAxisAlignment: state.conversation?.messages[i].sender.id != myId
+                ? MainAxisAlignment.start
+                : MainAxisAlignment.end,
             children: [
-              10.ph,
-              ProfilePicture(
-                uploadedFile: user.avatar,
-              ),
-              10.ph,
-              Text(
-                '${user.firstname} ${user.lastname}',
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                '@${user.username}',
-                style:
-                    const TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Container(
-                  color: MyColors.darkGrey,
-                  height: 1,
-                ),
-              ),
-              Expanded(
-                child: Container(
-                    color: Colors.red, child: Text('Chat content here')),
+              Column(
+                crossAxisAlignment:
+                    state.conversation?.messages[i].sender.id != myId
+                        ? CrossAxisAlignment.start
+                        : CrossAxisAlignment.end,
+                children: [
+                  _buildMessage(context, state.conversation?.messages[i], myId),
+                  Align(
+                    alignment: state.conversation?.messages[i].sender.id != myId
+                        ? Alignment.centerLeft
+                        : Alignment.centerRight,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      child: Text(
+                        _formatDate(state.conversation!.messages[i].sentAt),
+                        textAlign:
+                            state.conversation?.messages[i].sender.id != myId
+                                ? TextAlign.left
+                                : TextAlign.right,
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
+        ],
+      ],
+    );
+  }
+
+  bool _isSameDay(DateTime? date1, DateTime? date2) {
+    if (date1 == null || date2 == null) return false;
+    return date1.year == date2.year &&
+        date1.month == date2.month &&
+        date1.day == date2.day;
+  }
+
+  Widget _buildDaySeparator(BuildContext context, DateTime? date) {
+    if (date == null) {
+      return const SizedBox.shrink();
+    }
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Theme.of(context).colorScheme.onSurface,
+            width: MediaQuery.of(context).size.width * 0.3,
+            height: 1,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              _formatDate(date),
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onSurface,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            color: Theme.of(context).colorScheme.onSurface,
+            width: MediaQuery.of(context).size.width * 0.3,
+            height: 1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _formatDate(DateTime date) {
+    return '${date.month}/${date.day}/${date.year}';
+  }
+
+  Container _buildMessage(BuildContext context, message, int myId) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.7,
+      padding: const EdgeInsets.all(8),
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: message.sender.id != myId
+            ? Theme.of(context).colorScheme.secondary
+            : Theme.of(context).colorScheme.tertiary,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(
+            message.sender.id != myId ? 0 : 10,
+          ),
+          topRight: Radius.circular(
+            message.sender.id != myId ? 10 : 0,
+          ),
+          bottomRight: const Radius.circular(10),
+          bottomLeft: const Radius.circular(10),
+        ),
+      ),
+      child: Text(
+        message.message,
+        style: TextStyle(
+          color: message.sender.id != myId
+              ? Theme.of(context).colorScheme.onSecondary
+              : Theme.of(context).colorScheme.onTertiary,
         ),
       ),
     );
